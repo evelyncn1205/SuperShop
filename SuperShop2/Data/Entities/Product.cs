ca254c47de81a91ebfe23853e7663a0e99867e7a
Evelyncn1205 <evelynrx_rj@hotmail.com>
2023-06-06 18:17:13 +0100

    Init Project

---
+++ SuperShop2/Data/Entities/Product.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+using System;
+
+namespace SuperShop2.Data.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "The field {0} can contain {1} characters length.")]
+        public string Name { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
+        public decimal Price { get; set; }
+
+
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
+
+
+        [Display(Name = "Last Purchase")]
+        public DateTime? LastPurchase { get; set; }
+
+
+        [Display(Name = "Last Sale")]
+        public DateTime? LastSale { get; set; }
+
+
+        [Display(Name = "Is Available")]
+        public bool IsAvailable { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0:N2}", ApplyFormatInEditMode = false)]
+        public double Stock { get; set; }
+
+
+        //public User User { get; set; }
+
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImageUrl))
+                {
+                    return null;
+                }
+                return $"https://localhost:44394{ImageUrl.Substring(1)}";
+            }
+        }
+    }
+}
